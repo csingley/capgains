708b683e69a70c4d7e57489605121020af94d377
Christopher Singley <csingley@gmail.com>
2019-06-24 15:59:38 -0500

    utils - check in first_true()

--- capgains/utils.py
+++ capgains/utils.py
@@ -28,6 +28,21 @@ def all_equal(iterable):
     return next(g, True) and not next(g, False)
 
 
+def first_true(iterable, default=False, pred=None):
+    """Returns the first true value in the iterable.
+
+    If no true value is found, returns *default*
+
+    If *pred* is not None, returns the first item
+    for which pred(item) is true.
+
+    https://docs.python.org/3/library/itertools.html#itertools-recipes
+    """
+    # first_true([a,b,c], x) --> a or b or c or x
+    # first_true([a,b], x, f) --> a if f(a) else b if f(b) else x
+    return next(filter(pred, iterable), default)
+
+
 def matchEverything(element: Any) -> bool:
     """Degenerate predicate that always return True"""
     return True
