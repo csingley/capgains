c0b72cf7ebbd423e644c933e1d879602c7d176fb
Christopher Singley <csingley@gmail.com>
2019-06-24 09:13:18 -0500

    inventory.__init__ - use import * for component modules

--- capgains/inventory/__init__.py
+++ capgains/inventory/__init__.py
@@ -1,36 +1,6 @@
 # coding: utf-8
-from .types import (
-    Trade,
-    ReturnOfCapital,
-    Transfer,
-    Split,
-    Spinoff,
-    Exercise,
-    TransactionType,
-    Lot,
-    Gain,
-)
-from .api import (
-    Inconsistent,
-    UNITS_TOLERANCE,
-    Portfolio,
-    book,
-    book_model,
-    book_trade,
-    book_returnofcapital,
-    book_split,
-    book_transfer,
-    book_spinoff,
-)
-from .predicates import PredicateType, openAsOf, longAsOf, closable
-from .sortkeys import (
-    SortType,
-    sort_oldest,
-    sort_cheapest,
-    sort_dearest,
-    FIFO,
-    LIFO,
-    MINGAIN,
-    MAXGAIN,
-)
-from .functions import part_units, part_basis
+from .types import *
+from .api import *
+from .predicates import *
+from .sortkeys import *
+from .functions import *
